0354f6b4d7a45fd135d52d2cc11a3bd74204520e
Sindre Bøyum <boyum@users.noreply.github.com>
2021-10-06 10:32:22 +0200

    remove topic map specific files

--- src/semantics.ts
+++ src/semantics.ts
@@ -10,65 +10,7 @@ const semantics: Readonly<[H5PFieldGroup, H5PBehaviour, H5PL10n]> = [
     type: H5PFieldType.Group,
     widget: "timeline",
     importance: "high",
-    fields: [
-      {
-        label: "Topic map items",
-        name: "topicMapItems",
-        type: H5PFieldType.List,
-        entity: "Topic map item",
-        field: {
-          name: "topicMapItem",
-          type: H5PFieldType.Group,
-          fields: [
-            {
-              label: "Label",
-              description: "The label is shown on top of the background image",
-              name: "label",
-              type: H5PFieldType.Text,
-            },
-            {
-              label: "Background image",
-              name: "backgroundImage",
-              type: H5PFieldType.Image,
-            },
-            {
-              label: "Links",
-              name: "links",
-              description:
-                "These links are as auxiliary links for the user in the element's modal window",
-              type: H5PFieldType.List,
-              field: {
-                label: "Link",
-                name: "link",
-                type: H5PFieldType.Text,
-              },
-            },
-          ],
-        },
-      },
-      {
-        label: "Arrows",
-        name: "arrows",
-        type: H5PFieldType.List,
-        entity: "Arrow",
-        field: {
-          name: "arrow",
-          type: H5PFieldType.Group,
-          fields: [
-            {
-              name: "showStartHead",
-              type: H5PFieldType.Boolean,
-              widget: "none",
-            },
-            {
-              name: "showEndHead",
-              type: H5PFieldType.Boolean,
-              widget: "none",
-            },
-          ],
-        },
-      },
-    ],
+    fields: [],
   } as H5PFieldGroup,
   {
     name: "behaviour",
