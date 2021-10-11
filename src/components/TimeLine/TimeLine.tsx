883a636efa7e74961b5123102b4774cc91ba60bf
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2021-10-11 18:40:14 +0200

    fix lint

--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires, no-new */
 import * as React from "react";
 /// <reference types="timelinejs3" />
+const TL = require("@knight-lab/timelinejs");
 
 export type TimeLineProps = {
   timelineDefinition: TL.ITimelineConfig;
@@ -7,9 +9,7 @@ export type TimeLineProps = {
 
 export const TimeLine = (timelineProps: TimeLineProps): JSX.Element => {
   React.useEffect(() => {
-    const TL = require("@knight-lab/timelinejs");
     // Update the document title using the browser API
-    console.log(TL);
     new TL.Timeline("timeline-embed", timelineProps.timelineDefinition);
   });
 
