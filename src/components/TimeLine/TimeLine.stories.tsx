8f1c2a84dd1629e14fbcc438590fa7d61745f90e
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2021-10-13 16:27:23 +0200

    add grid as slide

--- src/components/TimeLine/TimeLine.stories.tsx
+++ src/components/TimeLine/TimeLine.stories.tsx
@@ -1,18 +1,56 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from "react";
+import ReactDOMServer from "react-dom/server";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { TimeLine, TimeLineProps } from "./TimeLine";
+import { Grid, GridProps } from "../Grid/Grid";
 
 export default {
   title: "Atoms/TimeLine",
   component: TimeLine,
 } as ComponentMeta<typeof TimeLine>;
 
+const gridArgs: GridProps = {
+  items: [
+    {
+      id: "1",
+      backgroundImage: {
+        path: "https://images.unsplash.com/photo-1569587112025-0d460e81a126?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
+        alt: "",
+      },
+      label: "Sheep in the distance",
+      links: [],
+      widthPercentage: 50,
+      heightPercentage: 75,
+      xPercentagePosition: 3,
+      yPercentagePosition: 5,
+    },
+    {
+      id: "2",
+      backgroundImage: {
+        path: "https://images.unsplash.com/photo-1533415648777-407b626eb0fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80",
+        alt: "",
+      },
+      label: "Sheep close up",
+      links: [],
+      widthPercentage: 30,
+      heightPercentage: 60,
+      xPercentagePosition: 50,
+      yPercentagePosition: 30,
+    },
+  ],
+};
+
 const defaultArgs: TimeLineProps = {
   timelineDefinition: {
-    events: [{ start_date: { year: 2021 }, text: { text: "test" } }],
-    title: { text: { text: "hei" } },
+    events: [
+      {
+        start_date: { year: 2021 },
+      },
+    ],
+    title: { text: { text: "<div id='timeline_title_slide'/>" } },
   },
+  title: <Grid {...gridArgs} />,
 };
 
 export const Empty: ComponentStory<typeof TimeLine> = () => {
--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,24 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-new */
 import * as React from "react";
-/// <reference types="timelinejs3" />
+import ReactDOM from "react-dom";
+
 const TL = require("@knight-lab/timelinejs");
 
 export type TimeLineProps = {
-  // @ts-expect-error TL is defined
-  timelineDefinition: TL.ITimelineConfig;
+  timelineDefinition: any;
+  title: JSX.Element;
 };
 
 export const TimeLine: React.FC<TimeLineProps> = ({
   timelineDefinition,
+  title,
 }: TimeLineProps) => {
   React.useEffect(() => {
     // Update the document title using the browser API
-    new TL.Timeline("timeline-embed", timelineDefinition);
-  });
+    new TL.Timeline("timeline-embed", timelineDefinition, { height: "100%" });
+    requestAnimationFrame(() => {
+      ReactDOM.render(title, document.getElementById("timeline_title_slide"));
+    });
+  }, [timelineDefinition]);
 
-  return (
-    <div id="timeline-embed">
-      <span>hello</span>
-    </div>
-  );
+  return <div id="timeline-embed" />;
 };
