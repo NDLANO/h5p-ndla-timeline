6cad65c4c350f816aa9346f684afd863b7bfa836
boyum <sindre.boyum@tietoevry.com>
2022-02-03 11:59:05 +0100

    add translate hook

---
+++ src/contexts/LocalizationContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+import { Translations } from "../types/Translations";
+
+export const LocalizationContext = createContext({} as Translations);
+export const L10nContext = LocalizationContext;
---
+++ src/hooks/useLocalization.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { L10nContext } from "../contexts/LocalizationContext";
+import { TranslationKey } from "../types/TranslationKey";
+
+export const useLocalization = (translationKey: TranslationKey): string => {
+  const translations = useContext(L10nContext);
+
+  return (
+    translations?.[translationKey] ?? `Missing translation: ${translationKey}`
+  );
+};
+
+export const useL10n = useLocalization;
