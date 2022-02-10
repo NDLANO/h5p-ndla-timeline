6b2de816bf1b0095a7fe02671ca9053a3ae87f8b
boyum <sindre.boyum@tietoevry.com>
2022-02-10 11:19:45 +0100

    remove unused code

--- src/types/OccupiedCell.ts
+++
@@ -1,9 +0,0 @@
-import { ElementType } from "./ElementType";
-
-export type OccupiedCell = {
-  occupiedById: string;
-  occupiedByType: ElementType;
-  x: number;
-  y: number;
-  index: number;
-};
