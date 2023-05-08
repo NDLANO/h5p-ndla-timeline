460ea8eeccfaeef1392acbbb63898c11c7582f26
Joseph Rézeau <joseph@rezeau.org>
2023-05-08 23:14:04 +0200

    under new construction
    Signed-off-by: Joseph Rézeau <joseph@rezeau.org>

--- src/utils/timeline.utils.tsx
+++ src/utils/timeline.utils.tsx
@@ -164,37 +164,7 @@ export const mapEventToTimelineSlide = (
   const endDate = event.endDate && parseDate(event.endDate);
   if (endDate) {
     slide.end_date = endDate;
-    console.log('startDate = ' + event.startDate + ' event.endDate = ' + event.endDate); 
-    // TODO papijo
-    // Check that end date if later than start date and issue alert if incorrect.
-    if (slide.start_date !== undefined) {
-      const startYear = (slide.start_date.year !== undefined) ? slide.start_date.year : undefined;
-      const endYear = (slide.end_date.year !== undefined) ? slide.end_date.year : undefined;
-      const startMonth = (slide.start_date.month !== undefined) ? slide.start_date.month : undefined;
-      const endMonth = (slide.end_date.month !== undefined) ? slide.end_date.month : undefined;
-      const startDay = (slide.start_date.day !== undefined) ? slide.start_date.day : undefined;
-      const endDay = (slide.end_date.day !== undefined) ? slide.end_date.day : undefined;
-      
-      if (startYear !== undefined 
-        && endYear !== undefined && startYear > endYear) {
-        console.error('End date ' + endYear + ' should be LATER than start date ' + startYear +' in Slide ' + event.title);          
-      }
-      else if (
-        startYear === endYear
-        && startMonth !== undefined
-        && endMonth !== undefined 
-        && startMonth > endMonth) {
-          console.error('End date ' + endYear + '-' + endMonth + ' should be LATER than start date ' + startYear + '-' + startMonth + ' in Slide ' + event.title);
-        }      
-      else if (
-        startYear === endYear
-        && startMonth === endMonth
-        && startDay !== undefined         
-        && endDay !== undefined 
-        && startDay > endDay ) {
-          console.error('End date ' + endYear + '-' + endMonth + '+' + endDay + ' should be LATER than start date ' + startYear + '-' + startMonth + '-' + endDay + ' in Slide ' + event.title);
-      }
-    };
+    // TODO rewrite this with Oliver's suggestions at https://github.com/NDLANO/h5p-ndla-timeline/pull/415
   }
 
   const media = getMedia(event);
