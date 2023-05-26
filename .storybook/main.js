2e5cafa6a42bfa148c0f0e09cd4abeac2b832a53
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-05-26 09:10:09 +0200

    build: update storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,39 +1,35 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const useLessLoader = require("storybook-less-loader");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  core: {
-    builder: "webpack5",
-  },
   webpackFinal: async config => {
     addScssSupport(config);
     useLessLoader(config);
     return config;
   },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
-
 function addScssSupport(config) {
   config.plugins.push(new MiniCssExtractPlugin());
-
   config.module.rules.push({
     test: /\.module.scss$/,
-    use: [
-      MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: true,
-        },
-      },
-      "sass-loader",
-    ],
+    use: [MiniCssExtractPlugin.loader, {
+      loader: "css-loader",
+      options: {
+        modules: true
+      }
+    }, "sass-loader"]
   });
-
   config.module.rules.push({
     test: /\.scss$/,
     use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-    exclude: /\.module\.scss$/,
+    exclude: /\.module\.scss$/
   });
 }
