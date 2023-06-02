63c4b412e7710eac33877a700eb677b452609923
Oliver Tacke <o.tacke@posteo.de>
2023-06-02 19:38:10 +0200

    Remove obsolete uuid fields

--- src/utils/timeline.utils.tsx
+++ src/utils/timeline.utils.tsx
@@ -15,6 +15,7 @@ import { EventItemType } from '../types/EventItemType';
 import { Params } from '../types/Params';
 import { SlideType } from '../types/SlideType';
 import { isDefined } from './is-defined.utils';
+import { H5P } from 'h5p-utils';
 
 const html = String.raw;
 
@@ -166,7 +167,7 @@ export const mapEventToTimelineSlide = (
 
   // The `layout-x` part of this ID is used for styling and must not be removed
   // before we find another way to change slide layouts
-  const id = `${event.id}_layout-${event.layout}`;
+  const id = `${H5P.createUUID()}_layout-${event.layout}`;
 
   const slide: TimelineSlide = {
     unique_id: id,
