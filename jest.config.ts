4b14aac3727df257e5fc37456ad993a3b53a0448
Oliver Tacke <o.tacke@posteo.de>
2026-03-25 16:54:20 +0100

    Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
 
   testEnvironment: 'jsdom',
